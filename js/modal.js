4e29416ed7e6eac590a62b609518795697fa06eb
CauaMelgarejo <146901976+CauaMelgarejo@users.noreply.github.com>
2025-05-04 17:30:20 -0300

    ajuste imagens e js

--- js/modal.js
+++ js/modal.js
@@ -4,8 +4,10 @@ function openModal(title, text, imgSrc, subtitle = "Subtítulo padrão") {
   document.getElementById('modal-text').textContent = text;
   document.getElementById('modal-img').src = imgSrc;
   document.getElementById('backdrop').style.display = 'flex';
+  document.body.classList.add('body-no-scroll');
 }
   function closeModal() {
     document.getElementById('backdrop').style.display = 'none';
+    document.body.classList.remove('body-no-scroll');
   }
 
